159013d1fb56621797838012ae64432376f27b9f
eddy121384-ui <eddy121384@gmail.com>
2026-09-25 20:17:00 +0800

    55: keep timelapse depth lightweight

--- scripts/timelapse_replay_main.gd
+++ scripts/timelapse_replay_main.gd
@@ -291,7 +291,8 @@ func _build_replay_piece_visuals() -> void:
 			piece.uv_points,
 			piece.source_texture,
 			timelapse_workspace_scale,
-			false
+			false,
+			PuzzlePieceVisualFactoryScript.DETAIL_LITE
 		)
 
 		timelapse_piece_visuals[int(piece.piece_index)] = group
